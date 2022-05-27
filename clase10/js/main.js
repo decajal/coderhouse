41443f6a7993d8f83af0240f950474ceb5cbee2a
Diego Cajal <decajal@gmail.com>
2022-05-27 09:47:24 -0300

    feat: clase10: localStorage

--- clase10/js/main.js
+++ clase10/js/main.js
@@ -4,10 +4,10 @@
     Clase 10: Storage & JSON
     Fecha 24/05/2022
 
-    Storage: nos permite almacenar datos de manera local del navegador sin necesidad de conexión con una BD
-    No manda mensajes al servidor (las cookies si por ejemplo)
-    Guarda información en mi navegador, entonces no se debe guardar información SENSIBLE
-    Lo que se guarda es información NO SENSIBLE que solo sirve para la interacción con el usuario
+    Storage: nos permite almacenar datos de manera local del navegador sin necesidad de un servidor o de conexión con una BD.
+    No manda mensajes al servidor (las cookies si por ejemplo) Storage y cookies son dos conceptos diferentes.
+    Guarda información en mi navegador, entonces no se debe guardar información SENSIBLE.
+    Lo que se guarda es información NO SENSIBLE que solo sirve para la interacción con el usuario.
 
     Los datos que se guardan en sesion y local son datos string
     + localStorage:
@@ -34,7 +34,43 @@ Se usa JSON como intermediario para guardar información (objetos) en el local s
     + localStorage.setItem('keyDelValor', JSON.stringify(valor)) --> pasar de Objeto a JSON
     + JSON.parse(localStorage.getItem('keyDelValor')) --> pasar de JSON a Objecto
 */
+// vamos a hacer un selector de Dark a Light mode
+let btDarkMode = document.getElementById('btDarkMode')
+let btLightMode = document.getElementById('btLightMode')
 
+let darkMode
+if (localStorage.getItem('darkMode')) {
+    darkMode = localStorage.getItem('darkMode')
+} else {
+    localStorage.setItem('darkMode', 'light')
+}
 
+if (darkMode == "dark") {
+    document.body.classList.add('darkMode')
+}
 
+/*
+// 1ra forma de hacerlo
+btDarkMode.addEventListener('click', () => {
+    // para no hacer de esta forma se hace en un archivo CSS
+    document.body.style.backgroundColor = "#000000"
+    document.body.style.color = "#ffffff"
+})
 
+btLightMode.addEventListener('click', () => {
+    // para no hacer de esta forma se hace en un archivo CSS
+    document.body.style.backgroundColor = "#ffffff"
+    document.body.style.color = "#000000"
+})
+*/
+
+// 2da forma de hacerlo
+btDarkMode.addEventListener('click', () => {
+    document.body.classList.add("darkMode")
+    localStorage.setItem('darkMode', 'dark')
+})
+
+btLightMode.addEventListener('click', () => {
+    document.body.classList.remove("darkMode")
+    localStorage.setItem('darkMode', 'light')
+})
