6f97df8480648f9c0a011da679d8d9ebf186ca7e
Diego Cajal Perez <DECajalPerez@teco.com.ar>
2022-06-14 20:29:56 -0300

    Pre-Feat: Falta ordenar código y el deleteAll

---
+++ Backend-36535/nodeJS/clase04_desafio/Contenedor.js
@@ -0,0 +1,91 @@
+const fs = require('fs');
+
+class Contenedor
+{
+    constructor(nombre)
+    {
+        this.nombre = nombre;
+    }
+
+    async save(objeto)
+    { 
+        try
+        {
+            const data = await this.getAll();
+            let idMax = 0;
+            data.forEach(element => {
+                if (element.id > idMax)
+                    idMax = element.id;
+            });
+            idMax++;
+            const newObject = { title: objeto.title, price: objeto.price, thumbnail: objeto.thumbnail, id: idMax }
+            //console.log(newObject);
+            data.push(newObject);
+            await fs.promises.writeFile(this.nombre, JSON.stringify(data)); // Si funciona !! Preguntar mañana como lo entrego
+            console.log(data);
+        }
+        catch (error)
+        {
+            return error;
+        }
+    }
+    async getById(id)
+    {
+        try
+        {
+            const data = await this.getAll();
+            const result = data.find(x => x.id === id);
+
+            return result ?? null;
+        }
+        catch (error)
+        {
+            return error;
+        }
+    }
+    async getAll()
+    {
+        try
+        {
+            const result = await fs.promises.readFile(this.nombre);
+            const data = await JSON.parse(result);
+            
+            return data;
+        }
+        catch (error)
+        {
+            return error
+        }
+    }
+    async deleteById(id)
+    {
+        try
+        {
+            if (isNaN(id)) throw "El id ingresado es inválido";
+
+            const data = await this.getAll();
+            const reg = data.find(x => x.id === id);
+            if (reg)
+            {
+                const newData = data.filter(x => x.id !== id);
+                // guardar el archivo
+                const result = await fs.promises.writeFile(this.nombre, JSON.stringify(newData));
+                //console.log(result);
+                return 'Registro Eliminado !!';
+            }
+            else
+                throw 'No existe un registro con el id ingresado';
+        }
+        catch (error)
+        {
+            return error
+        }
+    }
+    delelteAll()
+    {
+        // Elimina todos objetos presentes en el archivo
+    }
+
+}
+
+module.exports = {Contenedor}
--- Backend-36535/nodeJS/clase04_desafio/app.js
+++ Backend-36535/nodeJS/clase04_desafio/app.js
@@ -3,4 +3,59 @@
     decajalperez@teco.com.ar
     Desafío clase 04 - nodeJS
 
+    Consigna: Implementar programa que tenga una clase Contenedor que reciba el nombre del archivo
+    con el que va a trabajar e implemente los siguientes métodos:
+    + save(object): number - Recibe un objeto, lo guarda en el archivo, devuelve el id asignado.
+    + getById(number): object - Recibe un id y devuelve el objeto con ese id, o null si no está.
+    + getAll(): Object[] - Devuelve un array con todos los objetos presentes en el archivo.
+    + deleteById(number): void - Elimina del archivo el objeto con el id Buscado.
+    + deleteAll(): void - Elimina todos los objetos presentes en el archivo.
 */
+const { Contenedor } = require('./Contenedor.js');
+
+const cont = new Contenedor('./src/productos.txt');
+
+const product =
+    {
+        title: 'Cuaderno de notas',
+        price: 789,
+        thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png'
+    }
+cont.save(product)
+    .then(data =>
+        {
+            console.log(data);
+        })
+    .catch(err =>
+        {
+             console.log(err);
+        });
+
+
+// cont.getAll()
+//     .then(data => 
+//         {
+//             console.log(data);
+//         })
+//     .catch(err => 
+//         {
+//             console.log(err);
+//         });
+// cont.getById(1)
+//     .then(data => 
+//         {
+//             console.log(data);
+//         })
+//         .catch(err => 
+//         {
+//             console.log(err);
+//         });
+// cont.deleteById(5)
+//     .then(data => 
+//         {
+//             console.log(data);
+//         })
+//     .catch(err => 
+//         {
+//             console.log(err);
+//         });
