3a2634a9f2db8c0baa37c42d061811790638831a
Diego Cajal <decajal@gmail.com>
2022-06-16 09:49:10 -0300

    Refactor: del código de la clase 08 - Express Avanzado

--- Backend-36535/nodeJS/clase07/src/index.js
+++ Backend-36535/nodeJS/clase07/src/index.js
@@ -1,10 +1,15 @@
 const express = require ('express');
 const app = express();
-const path = require('path');
-// configuracion static
+
+// para que el servidor pueda interpretar en forma automática mensajes tipo JSON en formato urlencoded al recibirlos
+// se debe indicar en forma explicita agregando estas dos líneas de código:
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true })); // extended: true --> precisa que el objeto "req.body" contendrá valores de cualquier tipo en lugar de sólo cadenas.
+// si esa lpínea el servidor no sabrá como interpretar los msjs recibidos !!
+
+// Tomamos el "path" para la configuración de los directorios
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public'))); // configuracion del directorio "static"
 
 const frase = "Hola mundo como están";
 
@@ -25,18 +30,17 @@ app.get("/api/letras/:num", (req, res) =>
     else
     {
         if (req.params.num > frase.length)
-            res.send({error: "El parámetro está fuera de rango"})
+            res.send({error: "El parámetro está fuera de rango"});
         res.send(frase[parseInt(req.params.num)-1]);
     }
 });
 app.get("/api/palabras/:num", (req, res) =>
 {
     const arr = frase.split(" ");
-    if (parseInt(req.params.num) <= arr.length) {
+    if (parseInt(req.params.num) <= arr.length)
       res.send(frase.split(" ")[parseInt(req.params.num) - 1]);
-    } else {
-      res.send('Parametro mal ingresado')
-    }
+    else
+      res.send('Parametro mal ingresado');
 });
 
 app.get("/api/sumar/:valor1/:valor2/", (req, res) => {
@@ -57,10 +61,6 @@ app.get("/api/sumar/:valor1/:valor2/", (req, res) => {
     res.json({ resultado });
   });
   
-
-
-
-
 app.post('/', (req, res) =>
 {
     let data = req.body;
--- Backend-36535/nodeJS/clase08/index.js
+++ Backend-36535/nodeJS/clase08/index.js
@@ -4,9 +4,7 @@ const path = require('path');
 const indexRouter = require('./routes/indexRoutes');
 
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
-
-
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
