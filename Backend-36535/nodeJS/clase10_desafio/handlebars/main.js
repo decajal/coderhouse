2afa1cbaeb2d47fe9d1573fed472a1ff8bf789fd
Diego Cajal <decajal@gmail.com>
2022-06-22 18:15:48 -0300

    Feat: clase10_desafio handlebars listo, falta pug y ejs

--- Backend-36535/nodeJS/clase10_desafio/handlebars/main.js
+++ Backend-36535/nodeJS/clase10_desafio/handlebars/main.js
@@ -3,11 +3,10 @@ const handlebars = require('express-handlebars');
 const { engine } = handlebars;
 const app = express();
 const { body, validationResult } = require('express-validator');
+const productos = require('./models/modelProductos');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-
 
 app.engine(
     'hbs',
@@ -21,25 +20,55 @@ app.set('view engine', 'hbs');
 
 app.get('/', (req, res) =>
 {
-    //res.send('Hola Mundo !');
     res.render('formulario');
 });
 
-app.post('/productos', [
-    body('nombre', 'Mensaje de error').exists().isLength({min: 5}),
-    body('precio', 'Mensaje de Error').exists().isNumeric(),
+app.get('/productos', (req, res) =>
+{
+    const tieneElementos = (productos.length > 0) ? true : false;   
+    res.render('datos', { productos, tieneElementos });
+});
+
+app.post('/productos',
+[
+    body('nombre', 'Ingrese un nombre de producto válido (mínimo 5 caracteres)')
+        .exists().isLength({min: 5}),
+    body('precio', 'Ingrese un monto válido')
+        .exists().isNumeric(),
 ], (req, res) =>
+{
+    const errors = validationResult(req);
+    if (!errors.isEmpty())
     {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
+        {
+            const valores = req.body;
+            const errorsArray = errors.array();
+            return res.render('formulario', { errorsArray, valores, errors: true });
         }
-        const { nombre, precio, url } = req.body;
-        
-        res.redirect('/'); // <-- lo vuelve a mandar al formulario
-        
-        
+    }
+    const { nombre, precio, url } = req.body;
+    addProduct(nombre, precio, url);
+
+    res.redirect('/');
+});  
+
+const addProduct = (title, price, thumbnail) =>
+{
+    let idNew = maxId();
+    idNew++;
+    const newProduct = { title, price, thumbnail, id: idNew };
+    productos.push(newProduct);
+}
+
+const maxId = () =>
+{
+    let maxId = 0;
+    productos.forEach(element => {
+        if (element.id > maxId)
+            maxId = element.id;
     });
+    return maxId;
+}
 
 const server = app.listen(8080, () =>
                     {
