e45035c0b5fa1ff42b5cf039dab2450279dc2c43
Diego Cajal <decajal@gmail.com>
2022-06-23 20:38:21 -0300

    Feat: clase 10_desafio plantilla PUG funcionando

--- Backend-36535/nodeJS/clase10_desafio/pug/main.js
+++ Backend-36535/nodeJS/clase10_desafio/pug/main.js
@@ -1,6 +1,4 @@
 const express = require('express');
-const handlebars = require('express-handlebars');
-const { engine } = handlebars;
 const app = express();
 const { body, validationResult } = require('express-validator');
 const productos = require('./models/modelProductos');
@@ -10,15 +8,8 @@ const port = 8080;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.engine(
-    'hbs',
-    engine({
-        extname: '.hbs',
-        defaultLayout: 'index.hbs'
-    }));
-
 app.set('views', './views');
-app.set('view engine', 'hbs');
+app.set('view engine', 'pug');
 
 app.get('/', (req, res) =>
 {
@@ -77,4 +68,3 @@ const server = app.listen(port, () =>
                         console.log(`Servidor escuchando en el puerto ${server.address().port}`);
                     });
 server.on('error', error => console.log(`Error en el servidor ${error}`));
-
