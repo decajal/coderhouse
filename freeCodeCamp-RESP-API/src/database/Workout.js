6212801718db47ad94dd05a684e4ff96d2903a7b
Diego Cajal <decajal@gmail.com>
2022-07-08 09:51:46 -0300

    Feat: Trabajando sobre la capa de Servicios

---
+++ freeCodeCamp-RESP-API/src/database/Workout.js
@@ -0,0 +1,10 @@
+const DB = require('./db.json');
+
+const getAllWorkouts = () =>
+{
+    return DB.workouts;
+}
+
+module.exports = {
+    getAllWorkouts, 
+}
--- freeCodeCamp-RESP-API/src/v1/routes/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-
-router.route('/').get((req, res) =>
-{
-    res.send(`<h2>Hello from ${req.baseUrl}</h2>`);
-});
-
-module.exports = router;
