ce597a3b43491025c486b2984ea8b3c786a02bd4
Diego Cajal <decajal@gmail.com>
2022-07-14 18:53:03 -0300

    Feat: Login x Formulario, ahora usa Atlas, tiene un fix

--- Backend-36535/nodeJS/clase24_loginXformulario/app.js
+++ Backend-36535/nodeJS/clase24_loginXformulario/app.js
@@ -1,19 +1,39 @@
 require("dotenv").config();
+const path = require("path");
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
 const bodyParser = require("body-parser");
-const path = require("path");
+const storeMongo = require("connect-mongo");
+const { engine } = require("express-handlebars");
+const User = require("./models/User");
 
-const port = process.env.PORT || 8080;
+//const port = process.env.PORT || 8080;
 const app = express();
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-//app.use(express.static(path.join(__dirname, "public"))); // si la comento no me permite entrar en las paginas usando la url directamente al html
+
+// configuración de express-handlebars
+app.engine(
+  "hbs",
+  engine({
+    layoutsDir: path.join(__dirname, "views/layouts"),
+    defaultLayout: "index",
+    extname: ".hbs",
+  })
+);
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
 app.use(cookieParser());
+//const uri = process.env.MONGO_URI + "/" + process.env.MONGO_COLLECTION;
+const uri = process.env.MONGO_ATLAS;
 app.use(
   session({
+    store: storeMongo.create({
+      mongoUrl: uri,
+      mongoOptions: { useNewUrlParser: true, useUnifiedTopology: true },
+    }),
     key: "user_sid",
     secret: "c0d3r",
     resave: false,
@@ -22,9 +42,11 @@ app.use(
   })
 );
 
-app.listen(port, () => console.log(`Server ON, Port: ${port}`));
+app.listen(process.env.PORT, () =>
+  console.log(`Server ON, Port: ${process.env.PORT}`)
+);
 
-let users = [];
+//let users = [];
 
 const sessionChecker = (req, res, next) => {
   if (req.cookies.user_sid && req.session.user) {
@@ -39,43 +61,59 @@ app.get("/", sessionChecker, (req, res) => {
 });
 
 app.get("/login", sessionChecker, (req, res) => {
-  res.sendFile(path.join(__dirname, "public/login.html"));
+  res.render("login", { title: "Login de Usuario" });
 });
 
-app.post("/login", (req, res) => {
+app.post("/login", async (req, res) => {
   const { username, password } = req.body;
-  const userExist = users.find((e) => e.username === username);
+  const userExist = await User.findOne({ username: username });
+  //const userExist = users.find((e) => e.username === username);
   if (!userExist) return res.redirect("/login"); // El usuario no existe
 
-  if (userExist.password !== password) return res.redirect("/login"); // La contraseña ingresada no coincide
+  //if (userExist.password !== password) return res.redirect("/login"); // La contraseña ingresada no coincide
+  const result = userExist.comparePWD(password, userExist.password);
+  if (!result) return res.redirect("/login"); // La contraseña ingresada no coincide
 
   req.session.user = { username: userExist.username, email: userExist.email }; // registra usuario en session
   res.redirect("/dashboard");
 });
 
 app.get("/signup", sessionChecker, (req, res) => {
-  res.sendFile(path.join(__dirname, "public/signup.html"));
+  res.render("signup", { title: "Registro de Usuarios" });
 });
 
-app.post("/signup", (req, res) => {
+app.post("/signup", async (req, res) => {
   const { email, username, password } = req.body;
-  const userExist = users.find((e) => e.username === username);
+  const userExist = await User.findOne({ username: username });
+  //const userExist = users.find((e) => e.username === username);
   if (userExist) return res.send({ message: "El usuario ya existe !" });
-  const newUser = {
-    email,
-    username,
-    password,
-  };
-  users.push(newUser); // persistencia en memoria
-  res.redirect("/login");
+  // const newUser = {
+  //   username,
+  //   password,
+  //   email,
+  // };
+  //users.push(newUser); // persistencia en memoria
+  let newUser = new User({
+    username: username,
+    password: password,
+    email: email,
+  });
+  newUser.save((err, doc) => {
+    if (err) {
+      //console.log(err);
+      res.redirect("/signup");
+    } else res.redirect("/login");
+  });
 });
 
 app.get("/dashboard", (req, res) => {
   if (req.cookies.user_sid && req.session.user) {
-    res.sendFile(path.join(__dirname, "public/dashboard.html"));
-  } else {
-    res.redirect("/login");
-  }
+    res.render("dashboard", {
+      title: "Dashboard del usuario",
+      userName: req.session.user.username,
+      email: req.session.user.email,
+    });
+  } else res.redirect("/login");
 });
 
 app.post("/logout", (req, res) => {
---
+++ Backend-36535/nodeJS/clase24_loginXformulario/models/User.js
@@ -0,0 +1,41 @@
+require("dotenv/config");
+//const path = require("path");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+//const collectionsMongo = "dbUsers";
+//const uri = path.join(process.env.MONGO_URI, collectionsMongo); // con esto me da error
+//const uri = process.env.MONGO_URI + "/" + process.env.MONGO_COLLECTION;
+const uri = process.env.MONGO_ATLAS + "/" + process.env.MONGO_COLLECTION;
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const schema = new mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: [true, "name is required"],
+  },
+  password: {
+    type: String,
+    required: [true, "password is required"],
+  },
+  email: {
+    type: String,
+    required: [true, "email is required"],
+  },
+});
+
+schema.pre("save", function (next) {
+  if (this.isModified("password")) {
+    this.password = bcrypt.hashSync(this.password, 10);
+  }
+  next();
+});
+
+schema.methods.comparePWD = (password, hash) => {
+  const result = bcrypt.compareSync(password, hash);
+  return result;
+};
+
+module.exports = mongoose.model("User", schema);
